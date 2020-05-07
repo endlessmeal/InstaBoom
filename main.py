7a9acb937afef8ffe219e738d781597fcd7b39da
endlessmeal <baloyan99@list.ru>
2020-05-07 20:20:36 +0300

    added a possibility to set which hashtags bot must like

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import constants
 bot = telebot.TeleBot(constants.token)
 login = constants.login
 password = constants.password
-
+selectedOption = ''
+like_tag = ''
+option_name = ''
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -19,22 +21,33 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'лайк':
+        global option_name
+        option_name = 'хештег'
         bot.send_message(message.from_user.id, selected(message))
     elif message.text.lower() == 'комментарий':
+        global option_name
+        option_name = 'комментарий'
         bot.send_message(message.from_user.id, selected(message))
     elif message.text.lower() == 'подписка':
+        global option_name
+        option_name = 'количество'
         bot.send_message(message.from_user.id, selected(message))
 
 
 def selected(message):
+    global selectedOption
     bot.send_message(message.from_user.id, 'Вы выбрали ' + message.text + '\n' + 'Введите Логин')
+    selectedOption = message.text
     bot.register_next_step_handler(message, get_login)
+
+
+print(selectedOption)
 
 
 def get_login(message):
     global login
     login = message.text
-    bot.send_message(message.from_user.id, 'Введи пароль')
+    bot.send_message(message.from_user.id, 'Введите пароль')
     bot.register_next_step_handler(message, get_password)
 
 
@@ -42,17 +55,31 @@ def get_password(message):
     global password
     password = message.text
     bot.send_message(message.from_user.id, 'Авторизация, подождите')
-    bot.register_next_step_handler(message, authorization(message))
+    authorization(message)
 
 
 def authorization(message):
     print(login)
     print(password)
-    session = InstaPy(username=str(login), password=str(password), want_check_browser=False, headless_browser=False)
-    if session.login() == 'SUCCESS':
-        bot.send_message(message.from_user.id, 'Успешно')
+    print(message.text)
+    global session
+    session = InstaPy(username=str(login), password=str(password), want_check_browser=False, headless_browser=True)
+    if session.login():
+        bot.send_message(message.from_user.id, 'Успешно' + '\n' + 'Введите ' + option_name)
+        bot.register_next_step_handler(message, choice)
     else:
         bot.send_message(message.from_user.id, 'Что-то пошло не так')
+        bot.register_next_step_handler(message, get_login)
+
+
+def choice(message):
+    global like_tag
+    like_tag = message.text
+    print(like_tag)
+    if selectedOption.lower() == 'лайк':
+        bot.send_message(message.from_user.id, 'Бот ставит 30 лайков в час по хештегу: ' + str(like_tag))
+        session.set_quota_supervisor(enabled=True, peak_likes_hourly=30, peak_likes_daily=800)
+        session.like_by_tags([str(like_tag)], amount=10000)
 
 
 bot.polling(none_stop=True)
